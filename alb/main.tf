ba8fd399ecce066549cd087f75c2a962d6636e7f
Abdulwasiu <abdulwasiu037@gmail.com>
2025-04-22 15:41:28 +0100

    created alb module

--- alb/main.tf
+++ alb/main.tf
@@ -1,11 +1,11 @@
 # create application load balancer
 resource "aws_lb" "application_load_balancer" {
   name                       = "${var.project_name}-${var.environment}-alb"
-  internal                   = 
-  load_balancer_type         = 
-  security_groups            = 
-  subnets                    = 
-  enable_deletion_protection = 
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [var.alb_security_group.id]
+  subnets                    = [var.public_subnet_az1_id, var.public_subnet_az2_id] 
+  enable_deletion_protection = false
 
   tags = {
     Name = "${var.project_name}-${var.environment}-alb"
@@ -15,10 +15,10 @@ resource "aws_lb" "application_load_balancer" {
 # create target group
 resource "aws_lb_target_group" "alb_target_group" {
   name        = "${var.project_name}-${var.environment}-tg"
-  target_type = 
-  port        = 
-  protocol    = 
-  vpc_id      = 
+  target_type = var.target_type
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
 
   health_check {
     healthy_threshold   = 5
@@ -34,9 +34,9 @@ resource "aws_lb_target_group" "alb_target_group" {
 
 # create a listener on port 80 with redirect action
 resource "aws_lb_listener" "alb_http_listener" {
-  load_balancer_arn = 
-  port              = 
-  protocol          = 
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
     type = "redirect"
@@ -51,14 +51,14 @@ resource "aws_lb_listener" "alb_http_listener" {
 
 # create a listener on port 443 with forward action
 resource "aws_lb_listener" "alb_https_listener" {
-  load_balancer_arn = 
-  port              = 
-  protocol          = 
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 443
+  protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = 
+  certificate_arn   = var.certificate_arn
 
   default_action {
     type             = "forward"
-    target_group_arn = 
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
 }
