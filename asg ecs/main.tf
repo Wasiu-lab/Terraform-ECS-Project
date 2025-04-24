8dfcf063a879ac52266adffca561b48ac64c10f6
Abdulwasiu <abdulwasiu037@gmail.com>
2025-04-24 14:11:42 +0100

    created asg moduel

--- asg ecs/main.tf
+++ asg ecs/main.tf
@@ -1,19 +1,19 @@
 # create an auto scaling group for the ecs service
 resource "aws_appautoscaling_target" "ecs_asg" {
-  max_capacity       = 
-  min_capacity       = 
-  resource_id        = "service/${}-${}-cluster/${}-${}-service"
+  max_capacity       = 4
+  min_capacity       = 1
+  resource_id        = "service/${var.project_name}-${var.environment}-cluster/${var.project_name}-${var.environment}-service"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 
-  depends_on = 
+  depends_on = [var.ecs_service]
 }
 
 # create scaling policy for the auto scaling group
 resource "aws_appautoscaling_policy" "ecs_policy" {
-  name               = "${}-${}-policy"
+  name               = "${var.project_name}-${var.environment}-policy"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = "service/${}-${}-cluster/${}-${}-service"
+  resource_id        = "service/${var.project_name}-${var.environment}-cluster/${var.project_name}-${var.environment}-service"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
   
@@ -29,5 +29,5 @@ resource "aws_appautoscaling_policy" "ecs_policy" {
     disable_scale_in   = false 
   }
 
-  depends_on = 
+  depends_on = [aws_appautoscaling_target.ecs_asg]
 }
---
+++ asg ecs/variables.tf
@@ -0,0 +1,6 @@
+# asg variables
+variable "project_name" {}
+variable "environment" {}
+variable "ecs_service" {}
+
+
